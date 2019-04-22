5c74c5e5fd8c9fc4812d012200fbb80e2884ec3b
YEmreAk <yedhrab@gmail.com>
2019-04-22 19:42:34 +0300

    Gereksizler Kaldırıldı 🗑
    Ders notları https://github.com/yedhrab/IU-CE-DersNotlari sayfasına alındı

--- Ders Notları/Bilgisayar Mimarisi.md
+++
@@ -1,3 +0,0 @@
-# Bilgisayar Mimarisi
-
-Ders içerikleri için [buraya](https://drive.google.com/open?id=1UaEzFYthEzpGg_r3B0o4bznxK6XkuEVE) tıklayabilirsin.
--- git_remote_fixer.sh
+++
@@ -1,5 +0,0 @@
-git remote remove origin
-git remote add origin https://gitlab.com/yedhrab/bilgiler.git
-git remote set-url --add --push origin https://gitlab.com/yedhrab/bilgiler.git
-git remote set-url --add --push origin https://github.com/yedhrab/bilgiler.git
-git push origin master
